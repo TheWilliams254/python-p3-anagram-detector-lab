36aef6e8de3dea2f01da4e634621253a503f0f49
william <williamwambugu663@gmail.com>
2025-05-18 07:29:30 +0300

    anagram model

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        return [word for word in words if sorted(word) == sorted(self.word)]
+
+    def test_anagram(self):
+        listen = Anagram("listen")
+        assert(listen.match(["enlists", "google", "inlets", "banana"]) == ["inlets", "listen"])
